49ec775fd8bf9349966127778bf968ff75e072b8
remnightowl <repacuyan@nightowl.consulting>
2023-10-24 12:01:17 +0800

    sana naman

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -25,6 +25,8 @@ $app = new Illuminate\Foundation\Application(
 | incoming requests to this application from both the web and CLI.
 |
 */
+$app->usePublicPath(realpath(base_path('/../public_html')));
+
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
